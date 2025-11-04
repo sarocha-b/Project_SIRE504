d7d208244f4ac94ac8693becfa7c6d8076400fa8
sarocha <chunbunyasit.s@gmail.com>
2025-11-04 12:34:49 +0700

    add Filter function

--- src/seqQC/Filter/seqFilter.py
+++ src/seqQC/Filter/seqFilter.py
@@ -1,1 +1,16 @@
-# Seq Filter
+import pandas as pd
+
+def filterSeq(file):
+
+    seqtsv = pd.read_csv('stat_data.tsv', delimiter='\t')
+
+    seqtsv['FT_Mean'] = seqtsv['%Mean'] >= 70 
+    seqtsv['FT_Quartile'] = seqtsv['Quartile'] >= 30
+
+
+    seqtsv.to_csv(file, sep='\t', index=False )
+    
+    print(f"Finish")
+
+if __name__ == "__main__":
+    filterSeq('stat_data.tsv')
