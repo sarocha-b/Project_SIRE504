ce4376cb5c2d1926f24af5ea9090f8c544656014
jitchaw <jitprapas.cha@gmail.com>
2025-11-11 14:53:32 +0700

    update seqCal

--- src/seqQC/Calculation/seqCal.py
+++ src/seqQC/Calculation/seqCal.py
@@ -1,55 +1,46 @@
 import gzip
 from Bio import SeqIO
-import re
 import pandas as pd
-from functools import partial
 
 def cal_fastq_stat(file):
     with gzip.open(file, "rt") as handle:
         record_iterator = SeqIO.parse(handle, "fastq")
-        tsv_stat = []
-        for idx, rec in enumerate(record_iterator):
+        data_stat = []
+        for rec in record_iterator:
             total = 0
             seq_id = rec.id
             meanQ = sum(rec.letter_annotations['phred_quality'])/len(rec)
-            for k,l in rec.letter_annotations.items():
-                for t in l:
-                    if t >= 20:
-                        total += 1
-            pctQ20 = (total/len(rec))*100
+            # for k,l in rec.letter_annotations.items():
+            #     for t in l:
+            #         if t >= 20:
+            #             total += 1
+            # pctQ20 = (total/len(rec))*100
         
-            tsv_stat.append([seq_id, meanQ, pctQ20])
-            if idx == 20:
-                break
-    return tsv_stat
-    
+            data_stat.append([seq_id, meanQ])
+
+    return data_stat
+  
             
 
-def df_merge_stat(tsv_stat):
-    df_stat = pd.DataFrame(tsv_stat[0:], columns=['seq_id', 'meanQ', 'pctQ20'])
-    df_seqData = pd.read_table('/mnt/c/Users/jitpr/SIRE504_programming/project504/seq_20ex.tsv')
-    df_seqStat = pd.merge(df_seqData, df_stat, on='seq_id', how='inner')
+def df_merge_stat(data_stat):
+    df_stat = pd.DataFrame(data_stat[0:], columns=['seq_id', 'meanQ'])
+    df_seqStat = pd.merge(df, df_stat, on='seq_id', how='inner')
     return df_seqStat
     
 
-def calc_qt(df_seqStat):
-    q_30 = partial(pd.Series.quantile, q=0.30)
-    q_30.__name__ = "30%"
-    df_qt = df_seqStat.groupby('barcode')['meanQ'].agg([q_30])
-    df_seqStat = pd.merge(df_seqStat, df_qt, on='barcode', how='inner')
-    return df_seqStat
+# def calc_qt(df_seqStat):
+#     q_30 = partial(pd.Series.quantile, q=0.30)
+#     q_30.__name__ = "30%"
+#     df_qt = df_seqStat.groupby('barcode')['meanQ'].agg([q_30])
+#     df_seqStat = pd.merge(df_seqStat, df_qt, on='barcode', how='inner')
+#     return df_seqStat
    
-
-def save_seq_stat(df_seqStat):
-    df_seqStat.to_csv('sequence_stat.tsv', sep='\t', index=False)
-    print("save as sequence_stat.tsv")
-
 
 def calculate_sequence_statistics(file):
     tsv_stat = cal_fastq_stat(file)
     df_seqStat = df_merge_stat(tsv_stat)
-    df_seqStat = calc_qt(df_seqStat)
-    save_seq_stat(df_seqStat)
+  
+  
 
 if __name__ == "__main__":
     calculate_sequence_statistics('/mnt/c/Users/jitpr/SIRE504_programming/project504/ont_reads.project.fastq.gz')
