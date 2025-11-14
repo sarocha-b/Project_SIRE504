6c8468a0137d2ea31960d01735facdae1267289e
jitchaw <jitprapas.cha@gmail.com>
2025-11-14 16:28:58 +0700

    update ArgumentParser

--- src/seqQC/cli.py
+++ src/seqQC/cli.py
@@ -10,46 +10,16 @@ def argparserlocal():
 
     filter_command = subparsers.add_parser('main', help='Filter pass read')
     filter_command.add_argument("-f", "--file", type=str, default=None, 
-                                help="provide fastq file") 
-    filter_command.add_argument("-m", "--mins", action="store_true",
-                            help="Minimum read length")
-    filter_command.add_argument("-M", "--max", action="store_true",
-                            help="Maximum read length")
-    filter_command.add_argument("-q", "--qual", action="store_true",
-                                help="Minimum quality score")
-    filter_command.add_argument("-w", "--high", action="store_true",
-                                help="Maximum quality score")
+                                help="provide fastq.gz file") 
+    filter_command.add_argument("-m", "--mins", type=int, default=200,
+                            help="minimum read length")
+    # filter_command.add_argument("-M", "--max", action="store_true",
+    #                         help="Maximum read length")
+    filter_command.add_argument("-q", "--qual", type=int, default=7,
+                                help="minimum average quality score")
+    # filter_command.add_argument("-w", "--high", action="store_true",
+    #                             help="Maximum quality score")
 
     return parser
 
-def main():
-    parser = argparserlocal()
-    args = parser.parse_args()
 
-    # parser = argparserlocal()
-    # try:
-    #     args = parser.parse_args(argv)
-    # except SystemExit as e:
-    #     # Help (or parse error) triggered an exit; don't kill the process in this test.
-    #     # argparse has already printed the help/usage text.
-    #     print("============================\n")
-
-    # if args.file == None:
-    #     exit("------\nError: You do not provide -f or --file\n------\n")
-    # else:
-    #     file = args.file
-
-    if args.command == 'data_group_barcode':
-        if args.file == None:
-            parser.parse_args(['data_group_barcode','-h'])
-        file = args.file
-        data_group_barcode(file)
-
-    # if args.command == 'data_group_barcode':
-    #     if args.min == None:
-    #         parser.parse_args(['data_group_barcode','-h'])
-    #     print("Input",args.min,"\ngroup=", data_group_barcode(file) )
-
-if __name__ == "__main__":
-    # main(['-h'])
-    main()
--- src/seqQC/main_test.py
+++ src/seqQC/main_test.py
@@ -4,44 +4,23 @@ from cli import *
 from Data.seqData import *
 from Filter.seqFilter import *
 from Calculation.seqCal import *
-from src.main import main
+from main import *
 
 def main_test():
     parser = argparserlocal()
     args = parser.parse_args()
     
-    if args.file == None:
-        file = "test_data/test_data.fastq"
-    else:
-        file = args.file
-
-    if getattr(args, 'min', False) is True:
-        min_length = args.mins
-    else:
-        min_length = 200
-
-
+    file = args.file
+    min_length = args.mins
+    qscore = args.qual
+    
     if args.command == 'main':
-        if args.file == None:
+        if args.file == None or args.mins == None or args.qual == None:
             parser.parse_args(['main','-h'])
-        print("Input",args.file,"\nfilter=", main(file))
-    # elif args.command == 'filterData':
-    #     if args.min == None:
-    #         parser.parse_args(['filterData','-h'])  
-    #     print("input",args.mins,"\nMinimum length=", filterData(min_length, args.mins))
-    # elif args.command == 'filterData':
-    #     if args.max == None:
-    #         parser.parse_args(['filterData','-h'])  
-    #     print("input",args.max,"\nMaximum length=", filterData(df_seqStat, args.max))
-    # elif args.command == 'filterData':
-    #     if args.qual == None:
-    #         parser.parse_args(['filterData','-h'])  
-    #     print("input",args.qual,"\nMinimum quality score=", filterData(df_seqStat, args.qual))
-    # elif args.command == 'filterData':
-    #     if args.high == None:
-    #         parser.parse_args(['filterData','-h'])  
-    #     print("input",args.high,"\nMaximum quality score=", filterData(df_seqStat, args.high))
-        
+        print("Input",args.file,"\nStatus:", main(file, min_length, qscore))  
+        print("Minimum length:", args.mins)
+        print("Minimum average quality score:", args.qual)
+           
 
 if __name__ == "__main__":
     main_test() 
