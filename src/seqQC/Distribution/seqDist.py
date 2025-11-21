e4fdcada13d07336f98eca6264a267bde617c16d
sarocha <chunbunyasit.s@gmail.com>
2025-11-21 17:45:32 +0700

    edit

--- src/seqQC/Distribution/seqDist.py
+++ src/seqQC/Distribution/seqDist.py
@@ -3,7 +3,7 @@ import seaborn as sns
 # from Data.seqData import preprocess_data
 
 def plot_bc_dist(df_seqStat,column):
-    sns.displot(df_seqStat, x=column, kind="kde", hue="barcode")
+    sns.histplot(df_seqStat, x=column, bins=20, kde=True, hue="barcode")
     plt.title("Barcode Distribution")
     plt.show()
     return
