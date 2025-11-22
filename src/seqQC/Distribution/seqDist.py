0a449b91dbe19546b6717f4d75d40bfbd903c111
jitchaw <jitprapas.cha@gmail.com>
2025-11-22 20:26:07 +0700

    update seqDist

--- src/seqQC/Distribution/seqDist.py
+++ src/seqQC/Distribution/seqDist.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+from matplotlib.ticker import LogFormatter
 # from Data.seqData import preprocess_data
 
 def plot_bc_dist(df_seqStat,column):
-    sns.histplot(df_seqStat, x=column, bins=20, kde=True, hue="barcode")
+    ax = sns.histplot(df_seqStat, x=column, hue="barcode", bins=100, log_scale=True, element='poly')
+    ax.xaxis.set_major_formatter(LogFormatter(base=10, labelOnlyBase=False))
     plt.title("Barcode Distribution")
     plt.show()
     return
